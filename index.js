9821249602872dddfe1cc8dd0958dca61eaedc55
ulug'bek <azizbekovulugbek8@gmail.com>
2025-02-17 11:16:57 +0500

    none

---
+++ index.js
@@ -0,0 +1,10 @@
+let body = document.querySelector("body");
+let h1 = document.createElement("h1");
+
+h1.textContent = "Salom Dunyo!"
+
+body.appendChild(h1);
+
+body.style.backgroundColor = "blue";
+h1.style.color = "red";
+h1.style.textAlign = "center"
