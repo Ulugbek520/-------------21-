0d7cf07e478e470ca4bac4d43d0e61b68771646f
ulug'bek <azizbekovulugbek8@gmail.com>
2025-02-19 10:46:04 +0500

    none

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let body = document.querySelector("body");
-let h1 = document.createElement("h1");
+let h1 = document.querySelector("h1");
+let ism = prompt("Ismingizni kiriting: ");
+let rang = prompt("Yoqtirgan rangizni");
+let bg = prompt("Orga fon rangi: ");
 
-h1.textContent = "Salom Dunyo!"
-
-body.appendChild(h1);
-
-body.style.backgroundColor = "blue";
-h1.style.color = "red";
-h1.style.textAlign = "center"
+h1.textContent = `hello, ${ism}!`;
+h1.style.color = rang;
+h1.style.backgroundColor = bg;
+h1.style.textAligh = "center";
